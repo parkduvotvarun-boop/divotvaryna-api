3fbc52554467daa15c73fa840a6e7c9b9120e70d
parkduvotvarun-boop <parkduvotvarun@gmail.com>
2026-05-01 19:03:01 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -5,6 +5,7 @@ module.exports = async function handler(req, res) {
 
   if (req.method === "OPTIONS") return res.status(200).end()
 
+  
   if (req.method === "GET") {
     return res.status(200).json({ ok: true, message: "API works" })
   }
